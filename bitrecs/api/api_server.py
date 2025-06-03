907d349c65bf13bc894d6317c838e4a22d1fa51c
dmose <100089505+janusdotai@users.noreply.github.com>
2025-06-03 09:34:14 -0400

    removing counter, caching public key

--- bitrecs/api/api_server.py
+++ bitrecs/api/api_server.py
@@ -5,7 +5,6 @@ import bittensor as bt
 import hmac
 import hashlib
 import threading
-import asyncio
 from typing import Callable
 from functools import partial
 from fastapi import FastAPI, HTTPException, Request, APIRouter, Header
@@ -14,7 +13,6 @@ from fastapi.middleware.gzip import GZipMiddleware
 from bitrecs.utils import constants as CONST
 from bitrecs.commerce.product import ProductFactory
 from bitrecs.protocol import BitrecsRequest
-from bitrecs.api.api_counter import APICounter
 from bitrecs.api.api_core import filter_allowed_ips, limiter
 from bitrecs.api.utils import api_key_validator, get_proxy_public_key, json_only_middleware
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
@@ -67,8 +65,7 @@ class ApiServer:
         self.app.add_middleware(GZipMiddleware, minimum_size=500, compresslevel=5)
         self.app.add_exception_handler(Exception, general_exception_handler)
       
-        self.hot_key = validator.wallet.hotkey.ss58_address
-        self.proxy_public_key : bytes = None
+        self.hot_key = validator.wallet.hotkey.ss58_address        
         self.network = os.environ.get("NETWORK").strip().lower() #localnet / testnet / mainnet
 
         self.config = Config(
@@ -94,19 +91,13 @@ class ApiServer:
         self.app.include_router(self.router)
      
         try:
-            bt.logging.trace(f"\033[1;33mAPI warmup, please standby ...\033[0m")
-            self.proxy_public_key = get_proxy_public_key(PROXY_URL)
+            bt.logging.trace(f"\033[1;33mAPI warmup, please standby ...\033[0m")            
+            self.proxy_key : bytes = get_proxy_public_key(PROXY_URL)
+            self.public_key = Ed25519PublicKey.from_public_bytes(self.proxy_key)
         except Exception as e:
             bt.logging.error(f"\033[1;31mERROR API could not get proxy public key:  {e} \033[0m")
             bt.logging.warning(f"\033[1;33mWARNING - your validator is in limp mode, please restart\033[0m")
             raise Exception("Could not get proxy public key")
-        
-        #self.api_counter = APICounter(os.path.join(self.app.root_path, "api_counter.json"))
-        self.api_counter = APICounter(
-            os.path.join(self.app.root_path, "api_counter.json"),
-            auto_save_interval=60
-        )
-        bt.logging.info(f"\033[1;32m API Counter set {self.api_counter.save_path} \033[0m")
         
         bt.logging.info(f"\033[1;32m API Server initialized on {self.network} \033[0m")
 
@@ -165,10 +156,9 @@ class ApiServer:
 
         body_str = json.dumps(d, sort_keys=True)
         message = f"{x_timestamp}.{body_str}".encode('utf-8')
-        signature = bytes.fromhex(x_signature)
-        public_key = Ed25519PublicKey.from_public_bytes(self.proxy_public_key)
+        signature = bytes.fromhex(x_signature)        
         try:
-            public_key.verify(signature, message)
+            self.public_key.verify(signature, message)
         except InvalidSignature:
             bt.logging.error(f"\033[1;31m Invalid signature!\033[0m")
             raise HTTPException(status_code=401, detail="Invalid signature")
@@ -214,15 +204,13 @@ class ApiServer:
             catalog_size = len(store_catalog)
             bt.logging.trace(f"REQUEST CATALOG SIZE: {catalog_size}")
             if catalog_size < CONST.MIN_CATALOG_SIZE or catalog_size > CONST.MAX_CATALOG_SIZE:
-                bt.logging.error(f"API invalid catalog size")
-                await self.log_counter(False)
+                bt.logging.error(f"API invalid catalog size")                
                 return JSONResponse(status_code=400,
                                     content={"detail": "error - invalid catalog", "status_code": 400})            
             
             dupes = ProductFactory.get_dupe_count(store_catalog)
             if dupes > catalog_size * CONST.CATALOG_DUPE_THRESHOLD:
-                bt.logging.error(f"API Too many duplicates in catalog: {dupes}")
-                await self.log_counter(False)
+                bt.logging.error(f"API Too many duplicates in catalog: {dupes}")                
                 return JSONResponse(status_code=400,
                                     content={"detail": "error - dupe threshold reached", "status_code": 400})
 
@@ -231,8 +219,7 @@ class ApiServer:
             total_time = time.perf_counter() - st
 
             if len(response.results) == 0:
-                bt.logging.error(f"API forward_fn response has no results")
-                await self.log_counter(False)
+                bt.logging.error(f"API forward_fn response has no results")                
                 return JSONResponse(status_code=500,
                                     content={"detail": "error - forward", "status_code": 500})
 
@@ -253,19 +240,16 @@ class ApiServer:
                 "miner_hotkey": response.miner_hotkey,
                 "reasoning": f"Bitrecs AI - {self.network}"
             }
-
-            await self.log_counter(True)            
+            
             return JSONResponse(status_code=200, content=response)
         
         except HTTPException as h:
-            bt.logging.error(f"\033[31m HTTP ERROR API generate_product_rec_localnet:\033[0m {h}")
-            await self.log_counter(False)
+            bt.logging.error(f"\033[31m HTTP ERROR API generate_product_rec_localnet:\033[0m {h}")            
             return JSONResponse(status_code=h.status_code,
                                 content={"detail": "error", "status_code": h.status_code})
 
         except Exception as e:
-            bt.logging.error(f"\033[31m ERROR API generate_product_rec_localnet:\033[0m {e}")
-            await self.log_counter(False)
+            bt.logging.error(f"\033[31m ERROR API generate_product_rec_localnet:\033[0m {e}")            
             return JSONResponse(status_code=500,
                                 content={"detail": "error", "status_code": 500})
         
@@ -293,21 +277,18 @@ class ApiServer:
             catalog_size = len(store_catalog)
             bt.logging.trace(f"REQUEST CATALOG SIZE: {catalog_size}")
             if catalog_size < CONST.MIN_CATALOG_SIZE or catalog_size > CONST.MAX_CATALOG_SIZE:
-                bt.logging.error(f"API invalid catalog size")
-                await self.log_counter(False)
+                bt.logging.error(f"API invalid catalog size")                
                 return JSONResponse(status_code=400,
                                     content={"detail": "error - invalid catalog - size", "status_code": 400})
 
             dupes = ProductFactory.get_dupe_count_list(store_catalog)
             if dupes == -1:
-                bt.logging.error(f"API invalid catalog")
-                await self.log_counter(False)
+                bt.logging.error(f"API invalid catalog")                
                 return JSONResponse(status_code=400,
                                     content={"detail": "error - invalid catalog - format", "status_code": 400})
             
             if dupes > catalog_size * CONST.CATALOG_DUPE_THRESHOLD:
-                bt.logging.error(f"API Too many duplicates in catalog: {dupes}")
-                await self.log_counter(False)
+                bt.logging.error(f"API Too many duplicates in catalog: {dupes}")                
                 return JSONResponse(status_code=400,
                                     content={"detail": "error - dupe threshold reached", "status_code": 400})
 
@@ -318,8 +299,7 @@ class ApiServer:
             bt.logging.trace(response_text)
 
             if len(response.results) == 0:
-                bt.logging.error(f"API forward_fn response has no results")
-                await self.log_counter(False)
+                bt.logging.error(f"API forward_fn response has no results")                
                 return JSONResponse(status_code=500,
                                     content={"detail": "error - forward", "status_code": 500})
 
@@ -352,9 +332,7 @@ class ApiServer:
                 "miner_uid": response.miner_uid,
                 "miner_hotkey": response.miner_hotkey,
                 "reasoning": f"Bitrecs AI - {self.network}"
-            }
-
-            await self.log_counter(True)
+            }            
 
             et_a = int(time.time())
             total_duration = et_a - st_a
@@ -363,14 +341,12 @@ class ApiServer:
             return JSONResponse(status_code=200, content=response)
         
         except HTTPException as h:
-            bt.logging.error(f"\033[31m HTTP ERROR API generate_product_rec_testnet:\033[0m {h}")
-            await self.log_counter(False)
+            bt.logging.error(f"\033[31m HTTP ERROR API generate_product_rec_testnet:\033[0m {h}")            
             return JSONResponse(status_code=h.status_code,
                                 content={"detail": "error", "status_code": h.status_code})
 
         except Exception as e:
-            bt.logging.error(f"\033[31m ERROR API generate_product_rec_testnet:\033[0m {e}")
-            await self.log_counter(False)
+            bt.logging.error(f"\033[31m ERROR API generate_product_rec_testnet:\033[0m {e}")            
             return JSONResponse(status_code=500,
                                 content={"detail": "error", "status_code": 500})
         
@@ -404,14 +380,6 @@ class ApiServer:
             bt.logging.warning("API server is not running")
             return
         
-        #Counter stop
-        try:           
-            asyncio.run(self.api_counter.shutdown())
-            bt.logging.info("API counter shutdown complete")
-        except Exception as e:
-            bt.logging.error(f"Error during API counter shutdown: {e}")
-
-        #Server Stop
         self.server.should_exit = True
         self._server_thread.join(timeout=5)
         if self._server_thread.is_alive():
@@ -429,13 +397,3 @@ class ApiServer:
     def __exit__(self, exc_type, exc_val, exc_tb):
         """Context manager support"""
         self.stop()
-        
-
-    async def log_counter(self, success: bool) -> None:
-        try:
-            self.api_counter.update(success)
-            await self.api_counter.save_if_needed()
-        except Exception as e:
-            bt.logging.error(f"ERROR API could not update counter log: {e}")
-
-

b785ad8c9a218e12c7e20b8ddb32851f678e95e8
dmose <100089505+janusdotai@users.noreply.github.com>
2025-06-25 07:29:08 -0400

    ui fixes

--- bitrecs/utils/uids.py
+++ bitrecs/utils/uids.py
@@ -127,10 +127,10 @@ def ping_miner_uid(self, uid, port=8091, timeout=5) -> bool:
         sock.connect((ip, port))
         return True
     except ConnectionRefusedError:        
-        bt.logging.warning(f"Port {port} on IP {ip} is not connected.")
+        bt.logging.warning(f"Port {port} on for UID {uid} is not connected.")
         return False
     except socket.timeout:        
-        bt.logging.warning(f"Timeout on Port {port} on IP {ip}.")
+        bt.logging.warning(f"Timeout on Port {port} for UID {uid}.")
         return False
     except Exception as e:        
         bt.logging.error(f"An error occurred: {e}")
